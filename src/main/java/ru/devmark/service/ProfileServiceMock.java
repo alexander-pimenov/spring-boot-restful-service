89005e8298551309e9e6ea93eb0ed22e5cd1e8ad
Nord Mine <nordmine@gmail.com>
2018-01-05 01:25:56 +0300

    fix mock

--- src/main/java/ru/devmark/service/ProfileServiceMock.java
+++ src/main/java/ru/devmark/service/ProfileServiceMock.java
@@ -1,6 +1,7 @@
 package ru.devmark.service;
 
 import org.springframework.stereotype.Service;
+import ru.devmark.exception.ProfileNotFoundException;
 import ru.devmark.model.Profile;
 
 @Service
@@ -8,10 +9,15 @@ public class ProfileServiceMock implements ProfileService {
 
     @Override
     public Profile getProfile(int personId) {
-        Profile profile = new Profile();
-        profile.setId(123);
-        profile.setFirstName("Иван");
-        profile.setLastName("Иванов");
-        return profile;
+        // имитируем обращение к БД
+        if (personId == 123) {
+            Profile profile = new Profile();
+            profile.setId(personId);
+            profile.setFirstName("Иван");
+            profile.setLastName("Иванов");
+            return profile;
+        } else {
+            throw new ProfileNotFoundException(personId);
+        }
     }
 }
